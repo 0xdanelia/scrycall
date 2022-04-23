64e66d98233a8d63dc733af7554637ddebf1cdd8
0xdanelia <77199052+0xdanelia@users.noreply.github.com>
2022-04-23 18:51:41 -0400

    Replace tabs with spaces

--- source-old/scryapi.py
+++ source-old/scryapi.py
@@ -5,18 +5,18 @@ lastquerytime = 0
 
 # get the web address for calling the scryfall api
 def geturl(query):
-	return apiurl + urllib.parse.quote_plus(query)
+    return apiurl + urllib.parse.quote_plus(query)
 
 # call the api and return the data
 def getdata(url):
-	global lastquerytime
+    global lastquerytime
 
-	# don't query too often, be kind to the scryfall devs
-	diff = 0.1 - (time.time() - lastquerytime)
-	if diff > 0:
-		t1 = time.time()
-		time.sleep(diff)
-	
-	data = json.load(urllib.request.urlopen(url))
-	lastquerytime = time.time()
-	return data
+    # don't query too often, be kind to the scryfall devs
+    diff = 0.1 - (time.time() - lastquerytime)
+    if diff > 0:
+        t1 = time.time()
+        time.sleep(diff)
+    
+    data = json.load(urllib.request.urlopen(url))
+    lastquerytime = time.time()
+    return data
--- source/scrycall.py
+++ source/scrycall.py
@@ -7,17 +7,17 @@ from scry_output import print_cards
 
 
 def main():
-	
-	# query: string you would type in the search bar at scryfall.com
-	# flags: optional parameters
-	# formatting: the shape of the card data to display
-	query, flags, formatting = parse_args(sys.argv[1:])
-	# get cards
-	cards = get_cards_from_query(query)
-	# print results
-	print_cards(cards, formatting)
-	# exit
-	return 0
+    
+    # query: string you would type in the search bar at scryfall.com
+    # flags: optional parameters
+    # formatting: the shape of the card data to display
+    query, flags, formatting = parse_args(sys.argv[1:])
+    # get cards
+    cards = get_cards_from_query(query)
+    # print results
+    print_cards(cards, formatting)
+    # exit
+    return 0
 
 if __name__ == '__main__':
     sys.exit(main())
