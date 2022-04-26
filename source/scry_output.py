b0659f23af1d8038cff87952b33685444a5ff989
0xdanelia <77199052+0xdanelia@users.noreply.github.com>
2022-04-25 21:55:52 -0400

    Re-added logic to support '%{attribute.*}' operator. Currently only works as '.*' and not as the first character in an attribute like '%{*.data}'

--- source/scry_output.py
+++ source/scry_output.py
@@ -23,15 +23,14 @@ ATTR_CODES = {
 def print_data(data_list, format_string):
     print_lines = []
     for data in data_list:
-        print_line = get_print_line_from_data(data, format_string)
-        print_lines.append(print_line)
+        print_lines += get_print_lines_from_data(data, format_string)
 
     for line in print_lines:
         if line:
             print(line)
 
 
-def get_print_line_from_data(data, format_string):
+def get_print_lines_from_data(data, format_string):
     print_line = format_string
     # to handle multiple percent characters next to one another, replace '%%' with a unique placeholder first
     percent_placeholder = '[PERCENT_' + str(time.time()) + ']'
@@ -39,13 +38,14 @@ def get_print_line_from_data(data, format_string):
         percent_placeholder = '[PERCENT_' + str(time.time()) + ']'
     print_line = print_line.replace('%%', percent_placeholder)
 
-    print_line = substitute_attributes_for_values(print_line, data)
-    if print_line is None:
+    print_lines = substitute_attributes_for_values(print_line, data)
+    if not print_lines:
         return None
 
     # substitute the percent placeholder last
-    print_line = print_line.replace(percent_placeholder, '%')
-    return print_line
+    for i in range(len(print_lines)):
+        print_lines[i] = print_lines[i].replace(percent_placeholder, '%')
+    return print_lines
 
 
 def substitute_attributes_for_values(print_line, data):
@@ -53,19 +53,33 @@ def substitute_attributes_for_values(print_line, data):
     for attr_code in ATTR_CODES:
         print_line = print_line.replace(attr_code, ATTR_CODES[attr_code])
 
+    print_lines = []
+
     while True:
         # replace any '%{attribute}' strings with the correct value from the input data
         attribute_name = get_next_attribute_name(print_line)
         if attribute_name is None:
             # get_next_attribute_name() returns None when there are no more attributes to substitute
             break
-        attribute_value = get_attribute_value(attribute_name, data)
-        # if any attribute value is None, do not print anything on this line
-        if attribute_value is None:
-            return None
 
-        print_line = print_line.replace('%{' + attribute_name + '}', attribute_value)
-    return print_line
+        if '*' in attribute_name:
+            # '*' in an attribute can generate multiple lines
+            iterated_print_lines = iterate_attributes_in_print_line(print_line, attribute_name, data)
+            for line in iterated_print_lines:
+                print_lines += substitute_attributes_for_values(line, data)
+            return print_lines
+
+        else:
+            attribute_value = get_attribute_value(attribute_name, data)
+            # if any attribute value is None, do not print anything on this line
+            if attribute_value is None:
+                return []
+            print_line = print_line.replace('%{' + attribute_name + '}', str(attribute_value))
+
+    # even if only one line is printed, return it in a list so that it can be iterated in earlier functions
+    print_lines.append(print_line)
+
+    return print_lines
 
 
 def get_next_attribute_name(line):
@@ -100,3 +114,35 @@ def get_value_from_json_object(attr, data):
         if 0 <= idx < len(data):
             return data[idx]
     return None
+
+
+def iterate_attributes_in_print_line(print_line, attribute_name, data):
+    # for each possible value that '*' produces for a given attribute,
+    # create a new print_line, replacing the '*' with each possible individual value.
+
+    star_idx = attribute_name.find('.*')
+    sub_attr_name = attribute_name[:star_idx]
+    sub_attr_value = get_attribute_value(sub_attr_name, data)
+
+    iterated_lines = []
+
+    if isinstance(sub_attr_value, dict):
+        values_to_iterate = sub_attr_value.keys()
+    elif isinstance(sub_attr_value, list):
+        values_to_iterate = range(len(sub_attr_value))
+    else:
+        values_to_iterate = range(len(str(sub_attr_value)))
+
+    for sub_attr_value in values_to_iterate:
+        new_sub_attr_name = sub_attr_name + '.' + str(sub_attr_value)
+        # if the print_line contains duplicate sub-attributes, all will be replaced here
+        new_print_line = print_line.replace('%{' + sub_attr_name + '.*', '%{' + new_sub_attr_name)
+
+        if '*' in attribute_name[star_idx + 3:]:
+            # multiple stars in a single attribute are handled recursively
+            new_attribute_name = attribute_name.replace(sub_attr_name + '.*', new_sub_attr_name)
+            iterated_lines += iterate_attributes_in_print_line(new_print_line, new_attribute_name, data)
+        else:
+            iterated_lines.append(new_print_line)
+
+    return iterated_lines
