3dfdff3768c38ed53b5767ec50ac7ffdcbfa97b5
0xdanelia <77199052+0xdanelia@users.noreply.github.com>
2022-05-06 07:40:42 -0400

    Queries that return no results give a 404 error. This is caught and handled differently than other connection-related errors so that the query can be cached. This way, invalid queries that are called more than once will be read from the cache (and return no results) instead of constantly hitting the api.

--- source/scry_api.py
+++ source/scry_api.py
@@ -21,5 +21,15 @@ def get_api_data_from_url(url):
     else:
         GLOBAL_is_first_query = False
 
-    data = json.load(urllib.request.urlopen(url))
+    try:
+        with urllib.request.urlopen(url) as response:
+            data = json.load(response)
+    except urllib.error.HTTPError as exc:
+        if exc.code == 404:
+            # error code 404 means the query was processed, but it returned no results
+            # here we return None instead of raising an exception because we still want the 'bad' query to be cached
+            data = None
+        else:
+            raise exc
+
     return data
